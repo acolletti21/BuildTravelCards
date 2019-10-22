c1e4b52ada378ea07c8bdd480001c4594cd91c71
Ashley <ashleymcolletti@gmail.com>
2019-10-22 15:23:34 -0500

    cleanup

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import links from '../../links/links'
-import getImageName from '../../helpers/getImageName'
-import Card from '../Card'
 import './Explore.css'
 
 function Explore() {
   const listItems = links.map((link) =>
-    <li class="explore-links" key={link.name}><a href={link.url}>{link.name}</a>  {link.description}</li> 
+    <li className="explore-links" key={link.name}><a href={link.url}>{link.name}</a>  {link.description}</li> 
 );
   return (
     <>
       <h1>Explore</h1>
-      <div class="explore-container">
+      <div className="explore-container">
         <h4>Links to Help You Choose a Destination</h4>
         <div>
           {listItems}

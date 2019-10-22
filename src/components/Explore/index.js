3298c6840a305b9ff3e91ac041ca8989edbd530b
Ashley <ashleymcolletti@gmail.com>
2019-10-22 15:11:03 -0500

    Created Explore page content with links obj array
    -The explore page now contains a list of the objects from links.js
    -Links have a id, name, url, and description (feel free to add more)
    -Some styling added

--- src/components/Explore/index.js
+++ src/components/Explore/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import links from '../../links/links'
+import getImageName from '../../helpers/getImageName'
+import Card from '../Card'
+import './Explore.css'
 
-export default function Explore() {
+function Explore() {
+  const listItems = links.map((link) =>
+    <li class="explore-links" key={link.name}><a href={link.url}>{link.name}</a>  {link.description}</li> 
+);
   return (
     <>
       <h1>Explore</h1>
+      <div class="explore-container">
+        <h4>Links to Help You Choose a Destination</h4>
+        <div>
+          {listItems}
+        </div>
+      </div>
     </>
-  );
+  )
 }
+
+export default Explore
---
+++ src/links/links.js
@@ -0,0 +1,16 @@
+const links = [
+    {
+        id: 1,
+        name: "Google Maps",
+        url: "www.maps.google.com",
+        description: "Find a random point on the map and start exploring."
+    } ,
+    {
+        id: 2,
+        name: "SkyScanner",
+        url: "www.skyscanner.com",
+        description: "See what destinations have the cheapest flights"
+    }           
+]
+
+export default links
